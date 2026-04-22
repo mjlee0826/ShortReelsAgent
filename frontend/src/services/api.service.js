733aa2f958d3668643abbf7ac3b79517dcadaaa0
mjlee <yenchieh0826@gmail.com>
2026-04-22 16:19:34 +0800

    dir path bug

--- frontend/src/services/api.service.js
+++ frontend/src/services/api.service.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // 指向你的 FastAPI 後端
-const API_BASE_URL = 'http://localhost:8000/api';
+const API_BASE_URL = 'http://localhost:8001/api';
 
 class DirectorApiService {
   async generateTimeline(payload) {
